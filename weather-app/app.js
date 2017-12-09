996461cb6623eb17eb20aa32177f60b38155ddee
jason <jason.ch.ross@gmail.com>
2017-12-09 13:30:51 -0500

    Lecture 30 complete * Exploration of the request return objects, error, response and body. * Printing address and latitude of the given address (Baltimore City Hall)

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,7 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=100%20Holliday%20Street%20Baltimore',
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 4));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
