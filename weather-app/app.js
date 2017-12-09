bc349795e739b05800edbb76be661803a203d4b4
jason <jason.ch.ross@gmail.com>
2017-12-09 14:46:00 -0500

    Lecture 33 - Abstracting callbacks * Created geocode module with geocodeAddress method * Abstracted App.js request code and placed into geocodeAddress method * Called geocodeAddress with callback from App.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode.js');
 
 const argv = yargs
     .options({
@@ -14,23 +15,11 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.address)}`,
-    json: true
-}, (error, response, body) => {
-   if(error){
-       console.log('Unable to connect to Google servers');
-   }
-   else if(body.status === 'ZERO_RESULTS'){
-        console.log("Unable to find that address");
-   }
-   else if(body.status === 'OK'){
-       console.log(`Address: ${body.results[0].formatted_address}`);
-       console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-       console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-   }
-   else{
-       console.log("Unknown error");
-   }
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if(errorMessage){
+        console.log(errorMessage);
+    }
+    else{
+        console.log(JSON.stringify(results, undefined, 4));
+    }
 });
