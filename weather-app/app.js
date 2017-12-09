1d1be45cd3d206de5b275649fb80fe4b0baf2ae2
jason <jason.ch.ross@gmail.com>
2017-12-09 15:05:33 -0500

    Lecture 34 - Wiring up weather search * Use commercial darksky.net api to fetch weather for a static latitude/longitude coordinate pair

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,9 @@
 const yargs = require('yargs');
+const request = require('request');
 
 const geocode = require('./geocode/geocode.js');
 
-const argv = yargs
+/*const argv = yargs
     .options({
         address: {
             demand: true,
@@ -13,13 +14,27 @@ const argv = yargs
     })
     .help()
     .alias('help', 'h')
-    .argv;
+    .argv;*/
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+/*geocode.geocodeAddress(argv.address, (errorMessage, results) => {
     if(errorMessage){
         console.log(errorMessage);
     }
     else{
         console.log(JSON.stringify(results, undefined, 4));
     }
+});*/
+
+//https://api.darksky.net/forecast/eaf287b090aac83a10cfca7625fc18ba/37.8267,-122.4233
+request({
+    url: `https://api.darksky.net/forecast/eaf287b090aac83a10cfca7625fc18ba/39.287866,-76.592166499999`,
+    json: true
+}, (error, response, body) => {
+    if(!error && response.statusCode === 200){
+        console.log(body.currently.temperature);
+    }
+    else{
+        console.log('unable to fetch weather');
+    }
+
 });
