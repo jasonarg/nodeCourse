39c0e6be045293bbc1f920a4bff723d61819667d
jason <jason.ch.ross@gmail.com>
2017-12-09 13:15:02 -0500

    Lecture 29 complete * Simple demo of pretty print by using JSON.stringify to view all sub objects.

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,5 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=100%20Holliday%20Street%20Baltimore',
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(JSON.stringify(body, undefined, 4));
 });
