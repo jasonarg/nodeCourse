aa3c2d8f27efa12f790eddd9208ac77647ba80ed
jason <jason.ch.ross@gmail.com>
2017-12-10 17:21:56 -0500

    Lecture 45 - Express Middleware * Set up a maintenance hbs view * Setup a maintenance middleware that did not call next * Played with ordering of middleware calls to ensure static files also were affected by maintenance middleware
    # Next few lessons are on git and github and deployment. Nothing to commit

--- node-webserver/server.js
+++ node-webserver/server.js
@@ -1,20 +1,40 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
-app.set('view engine', 'hbs');
-
-
-app.use(express.static(__dirname + '/public'));
-
 hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
-
 hbs.registerHelper('screamIt', (text) => {
     return text.toUpperCase();
 });
 
+app.set('view engine', 'hbs');
+
+//Middleware
+
+app.use((req, res, next)=>{
+    var now = new Date().toString();
+    var logLine = `${now}: ${req.method} ${req.url}`;
+    console.log(logLine);
+    fs.appendFile('server.log', logLine + '\n', (err) => {
+        if(err){
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+
+/*app.use((req, res, next)=>{
+    res.render('maintenance.hbs');
+});*/
+
+app.use(express.static(__dirname + '/public'));
+
+
+//Routes
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Home Page',
@@ -28,7 +48,6 @@ app.get('/about', (req, res) => {
     });
 });
 
-// /bad  - send back json with errorMessage
 
 app.get('/bad', (req, res) =>{
    res.send({
@@ -36,7 +55,7 @@ app.get('/bad', (req, res) =>{
    });
 });
 
-
+//Start webserver
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000');
 });
