652aaa967316eb1b03f49cadd58b6562fdbb1abe
jason <jason.ch.ross@gmail.com>
2017-12-10 10:13:12 -0500

    Lecture 37 - Advanced Promises * Chained promises together * Used catch method on chained promise.then calls to resolve any error along the promise chain. * Implemented promise based geocoding functionality

--- weather-app/playground/promise.js
+++ weather-app/playground/promise.js
@@ -1,3 +1,24 @@
+var asyncAdd = (a,b) => {
+    return new Promise((resolve, reject) => {
+       setTimeout(() =>{
+           if(typeof a === 'number' && typeof b === 'number'){
+               resolve(a + b);
+           }
+           reject('Arguments must be numbers');
+       }) , 1500
+    });
+};
+
+asyncAdd('1', 3).then((res)=>{
+    console.log('Result: ', res);
+    return asyncAdd(res, 30);
+}).then((res)=>{
+    console.log('Should be 34:', res)
+}).catch((errorMessage) => {
+    console.log(errorMessage);
+});
+
+/*
 var somePromise = new Promise((resolve, reject) => {
     setTimeout(()=>{
         reject('Unable to fulfill promise');
@@ -10,4 +31,4 @@ somePromise.then((message) => {
     }, (errorMessage) => {
         console.log('Error: ', errorMessage);
     }
-);
+);*/
