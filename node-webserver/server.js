f28293063950b24f526c7dbdae8c81c522e70bd6
jason <jason.ch.ross@gmail.com>
2017-12-10 15:57:08 -0500

    Lecture 42 - Creating a Web Server * Setup static folder * Made basic html file to serve from it * Added callback to app.listen to log when the server initiation is complete

--- node-webserver/server.js
+++ node-webserver/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
+
 app.get('/', (req, res)=>{
 /*
     res.send('<h1>Hello Express!</h1>');
@@ -28,4 +31,6 @@ app.get('/bad', (req, res) =>{
 });
 
 
-app.listen(3000);
+app.listen(3000, ()=>{
+    console.log('Server is running on port 3000');
+});
