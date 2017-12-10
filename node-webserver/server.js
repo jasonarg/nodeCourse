352049f92b692d60a429aa1b2897b32faae6e89f
jason <jason.ch.ross@gmail.com>
2017-12-10 16:20:04 -0500

    Lecture 43 - Rendering Templates with Data * Installed hbs * Registered hbs as the view engine of the express server * Made two basic handlebars view templates * Setup a few basic routes to the hbs templates

--- node-webserver/server.js
+++ node-webserver/server.js
@@ -1,25 +1,27 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
+
+
+app.set('view engine', 'hbs');
 
 app.use(express.static(__dirname + '/public'));
 
 
-app.get('/', (req, res)=>{
-/*
-    res.send('<h1>Hello Express!</h1>');
-*/
-res.send({
-    name: 'Jason',
-    likes: [
-        'Pizze',
-        'Harney'
-    ]
-});
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Welcome to the home page'
+    });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 // /bad  - send back json with errorMessage
