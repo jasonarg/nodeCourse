1f131c607746efa0498db509747ce6dbe330bd82
jason <jason.ch.ross@gmail.com>
2017-12-10 22:20:37 -0500

    Lecture 53 -Using an Assertion Library * Installed older version of expect (now part of jest) * Made assertions on equality, types, and inclusion

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,17 +1,45 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers and return them', () => {
     var res = utils.add(33, 11);
 
-    if(res !== 44) {
-        throw new Error(`Expected: 44, but got ${res}`);
-    }
+    expect(res).toBe(44).toBeA('number');
+
 });
 
 it('should square a given integer', () => {
     var res = utils.square(5);
 
-    if(res !== 25) {
-        throw new Error(`Expected: 25, but got ${res}`);
-    }
+    expect(res).toBe(25).toBeA('number');
 });
+
+it('should set firstName and lastName', () => {
+    var user = {
+        location: 'Philadelphia',
+        age: 25
+    };
+    var res = utils.setName(user, 'Jason Ross');
+
+    expect(res).toInclude({
+        firstName: 'Jason',
+        lastName: 'Ross'
+    });
+
+});
+
+/*
+it('should expect some values', () => {
+    expect(12).toBe(12);
+    expect({name: 'Jason'}).toEqual({name: 'Jason'});
+    expect([2,3,4]).toInclude(4).toExclude(1);
+    expect({
+        name: 'Jason',
+        age: 37,
+        location: 'Baltimore'
+    }).toInclude({
+        age: 37
+    });
+
+});*/
