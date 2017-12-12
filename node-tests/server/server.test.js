4ff3561aa50a53f13ab290807d90b9d7beb4fc91
jason <jason.ch.ross@gmail.com>
2017-12-12 08:02:55 -0500

    Lecture 57 Organizing Test with describe() * Using nested describes to organize and label test output

--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -4,30 +4,35 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', (done) => {
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect((res) => {
-            expect(res.body).toInclude({
-               error: 'Page not found.'
-            });
-        })
-        .end(done);
-});
 
-//make a new test
-// assert 200 status code
-// user object exists with first name
-it('should return an array of users, with a user named jason', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body.users).toInclude({
-                name: 'Auttie',
-                age: 4
-            })
-        })
-        .end(done);
+describe('Server', () =>{
+    describe('GET /', () => {
+        it('should return hello world response', (done) => {
+            request(app)
+                .get('/')
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: 'Page not found.'
+                    });
+                })
+                .end(done);
+        });
+
+    });
+
+    describe('GET /users', () => {
+        it('should return an array of users, with a user named jason', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body.users).toInclude({
+                        name: 'Auttie',
+                        age: 4
+                    })
+                })
+                .end(done);
+        });
+    });
 });
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -2,31 +2,41 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers and return them', () => {
-    var res = utils.add(33, 11);
+describe('Utils', () => {
 
-    expect(res).toBe(44).toBeA('number');
+    describe('#add', () => {
 
-});
+        it('should add two numbers and return them', () => {
+            var res = utils.add(33, 11);
 
-it('should async add two numbers and return them', (done) => {
-    utils.asyncAdd(4, 3, (sum) => {
-        expect(sum).toBe(7).toBeA('number');
-        done();
+            expect(res).toBe(44).toBeA('number');
+
+        });
     });
-});
 
-it('should square a given integer', () => {
-    var res = utils.square(5);
-
-    expect(res).toBe(25).toBeA('number');
-});
-
-it('should async square a number', (done) => {
-    utils.asyncSquare(6, (res) => {
-        expect(res).toBe(36).toBeA('number');
-        done();
+    it('should async add two numbers and return them', (done) => {
+        utils.asyncAdd(4, 3, (sum) => {
+            expect(sum).toBe(7).toBeA('number');
+            done();
+        });
     });
+
+    it('should square a given integer', () => {
+        var res = utils.square(5);
+
+        expect(res).toBe(25).toBeA('number');
+    });
+
+    it('should async square a number', (done) => {
+        utils.asyncSquare(6, (res) => {
+            expect(res).toBe(36).toBeA('number');
+            done();
+        });
+
+    });
+
+
+
 
 });
 
@@ -43,7 +53,6 @@ it('should set firstName and lastName', () => {
     });
 
 });
-
 /*
 it('should expect some values', () => {
     expect(12).toBe(12);
